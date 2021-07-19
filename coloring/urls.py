b675e1a57c0982b65871001229193a23ca155f6a
awpierce <awpierce@berkeley.edu>
2021-07-18 22:31:13 -0700

    multiple changes

--- coloring/urls.py
+++ coloring/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('account/', views.account, name='account'),
     path('setting/', views.setting, name='setting'),
     path('reference/', views.reference, name='reference'),
+    path('asreference/', views.reference, name='asreference'),
     path('sketch/', views.sketch, name='sketch'),
+    path('mandela/', views.mandela, name='mandela'),
+    path('sketch_as_reference/', views.sketch_as_reference, name='sketch_as_reference'),
     path('sketch_with_reference/', views.sketch_with_reference, name='sketch_with_reference')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -12,11 +12,20 @@ def account(request):
 def setting(request):
     return render(request, 'coloring/setting.html')
 
+def mandela(request):
+    return render(request, 'coloring/mandela.html')
+
 def reference(request):
     return render(request, 'coloring/reference.html')
 
+def asreference(request):
+    return render(request, 'coloring/asreference.html')
+
 def sketch(request):
     return render(request, 'coloring/sketch.html')
+
+def sketch_as_reference(request):
+    return render(request, 'coloring/sketch_as_reference.html')
 
 def sketch_with_reference(request):
     return render(request, 'coloring/sketch_with_reference.html')
